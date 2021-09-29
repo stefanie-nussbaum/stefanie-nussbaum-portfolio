faea05a56e14289c9fb6b452019b3c67f305dd10
Stefanie <stefanien001@gmail.com>
2021-09-29 11:59:46 -0500

    added linked in and github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div>
-
+    <div className="footer-container">
+      <div className="footer">
+        <h4>by Stefanie Nussbaum</h4>
+        <Link to={{ pathname: "https://github.com/stefanie-nussbaum" }} target="_blank" >
+          <img className="icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" alt="github" />
+        </Link>
+        <Link to={{ pathname: "https://www.linkedin.com/in/stefanie-nussbaum-9a9369135/" }} target="_blank" >
+          <img className="icon" src="http://simpleicon.com/wp-content/uploads/linkedin.png" alt="linkedin" />
+        </Link>
+      </div>
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Main() {
   return (
     <div className='main'>
+      <div className='container'>
+        <div>
+          <Link></Link>
+        </div>
+      </div>
       Site Coming Soon!
 
     </div>
